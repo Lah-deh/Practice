f313a0ae632f24ead7c781355743587d1cd232ae
lah-deh <succssasokere@gmail.com>
2025-06-12 18:47:21 -1100

    debugged

--- practicee/vite.config.js
+++ practicee/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/,",
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "Practice"
+
 })
